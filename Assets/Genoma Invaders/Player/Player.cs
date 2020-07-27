918729f268e09b60011f01a9d7e6806ad0073210
Alberto Fernandez <albertofdzm@gmail.com>
2020-07-27 21:54:46 +0200

    feat(player): add Player Update method with movement logic

--- Assets/Genoma Invaders/Player/Player.cs
+++ Assets/Genoma Invaders/Player/Player.cs
@@ -2,4 +2,19 @@
 
 public class Player : MonoBehaviour
 {
+    void Update()
+    {
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Vector3-right.html
+        Vector3 right = Vector3.right;
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Time-deltaTime.html
+        float timeSinceLastFrame = Time.deltaTime;
+
+        Vector3 translation = right * timeSinceLastFrame;
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Transform.Translate.html
+        transform.Translate(
+          translation
+        );
+    }
 }
