3479f6c432fddbcdcc335ba7e001c824f3da1e02
Alberto Fernandez <albertofdzm@gmail.com>
2020-07-28 23:34:11 +0200

    feat(player): update Player component to handle horizontal axis input

--- Assets/Genoma Invaders/Player/Player.cs
+++ Assets/Genoma Invaders/Player/Player.cs
@@ -4,13 +4,16 @@ public class Player : MonoBehaviour
 {
     void Update()
     {
-        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Vector3-right.html
-        Vector3 right = Vector3.right;
+        // https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
+        float rawHorizontalAxis = Input.GetAxisRaw("Horizontal");
+
+        Vector3 direction = Vector3.zero;
+        direction.x = rawHorizontalAxis;
 
         // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Time-deltaTime.html
         float timeSinceLastFrame = Time.deltaTime;
 
-        Vector3 translation = right * timeSinceLastFrame;
+        Vector3 translation = direction * timeSinceLastFrame;
 
         // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Transform.Translate.html
         transform.Translate(
