5d217a4c709c7e7404418cea6535d8fdeeaee01c
Alberto Fernandez <albertofdzm@gmail.com>
2020-08-01 20:07:58 +0200

    feat(enemy): add Enemy component

---
+++ Assets/Genoma Invaders/Enemy/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
