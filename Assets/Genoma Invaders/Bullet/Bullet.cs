cde439a2bb3d28ffb002e755dcdd08620d11de64
Alberto Fernandez <albertofdzm@gmail.com>
2020-08-01 18:34:42 +0200

    feat(bullet): add Bullet Component

---
+++ Assets/Genoma Invaders/Bullet/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+
+    void Update()
+    {
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Vector3-up.html
+        Vector3 direction = Vector3.up;
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Time-deltaTime.html
+        float timeSinceLastFrame = Time.deltaTime;
+
+        Vector3 translation = direction * speed * timeSinceLastFrame;
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Transform.Translate.html
+        transform.Translate(
+          translation
+        );
+    }
+}
