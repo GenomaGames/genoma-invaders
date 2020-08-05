e1a816b9f92ad172790223b9461dd073cc2845a8
Alberto Fernandez <albertofdzm@gmail.com>
2020-08-05 22:31:21 +0200

    fix(workflow): update update-next-series-script to handle correctly branches

--- .github/scripts/update-next-series-script.js
+++ .github/scripts/update-next-series-script.js
@@ -1,27 +1,27 @@
 module.exports = async ({ github, context, core, io }) => {
   const owner = context.repo.owner;
   const repo = context.repo.repo;
-  const head = context.ref;
+  const { data: headBranch } = await await github.repos.getBranch({
+    owner,
+    repo,
+    branch: context.ref,
+  });
+  const head = headBranch.name;
   const branchSections = head.split("/");
-  console.log(branchSections);
   const padding = branchSections[1].length;
   const next = Number.parseInt(branchSections[1]) + 1;
   const nextId = next.toString().padStart(padding, "0");
   const base = `${branchSections[0]}/${nextId}`;
-  const { data: branch } = await await github.repos.getBranch({
+  const { data: baseBranch } = await await github.repos.getBranch({
+    owner,
+    repo,
+    branch: base,
+  });
+
+  return github.repos.merge({
     owner,
     repo,
     base,
+    head,
   });
-
-  if (branch) {
-    return github.repos.merge({
-      owner,
-      repo,
-      base,
-      head,
-    });
-  } else {
-    return null;
-  }
 };
