51204d73b84d6627fd861dd3da7da75aa06e339e
Alberto Fernandez <albertofdzm@gmail.com>
2020-08-05 21:48:17 +0200

    feat(branch): add github workdlow to update branches

---
+++ .github/scripts/update-next-series-script.js
@@ -0,0 +1,27 @@
+module.exports = async (github, context, core, io) => {
+  const owner = context.repo.owner
+  const repo = context.repo.repo
+  const head = context.ref
+  const branchSections = head.split('/')
+  const padding = branchSections[1].length
+  const next = Number.parseInt(branchSections[1]) + 1
+  const nextId = (next).toString().padStart(padding, '0')
+  const base = `${branchSections[0]}/${nextId}`
+  const { data: branch } = await await github.repos.getBranch({
+    owner,
+    repo,
+    base
+  })
+
+  if (branch) {
+    return github.repos.merge({
+      owner,
+      repo,
+      base,
+      head
+    })
+  } else {
+    return null;
+  }
+
+}
