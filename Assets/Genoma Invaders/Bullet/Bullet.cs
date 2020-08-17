22b04d0546bd3b3e4ac389a38fd3456bfa299e3f
Alberto Fernandez <albertofdzm@gmail.com>
2020-08-17 12:29:48 +0200

    feat(bullet): update logic to move using Rigidbody2D.MovePosition

--- Assets/Genoma Invaders/Bullet/Bullet.cs
+++ Assets/Genoma Invaders/Bullet/Bullet.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+// https://docs.unity3d.com/2020.1/Documentation/ScriptReference/RequireComponent.html
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
@@ -8,27 +9,27 @@ public class Bullet : MonoBehaviour
 
     // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Rigidbody2D.html
     private Rigidbody2D rb2D;
+    // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Vector3-up.html
+    private Vector3 move = Vector3.up;
 
     // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.Awake.html
-    private void Awake () {
+    private void Awake()
+    {
         // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Component.GetComponent.html
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.Update.html
-    private void Update()
+    // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.FixedUpdate.html
+    private void FixedUpdate()
     {
-        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Vector3-up.html
-        Vector3 direction = Vector3.up;
+        if (move != Vector3.zero)
+        {
+            // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Time-fixedDeltaTime.html
+            Vector3 translation = move * speed * Time.fixedDeltaTime;
+            Vector3 newPosition = transform.position + translation;
 
-        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Time-deltaTime.html
-        float timeSinceLastFrame = Time.deltaTime;
-
-        Vector3 translation = direction * speed * timeSinceLastFrame;
-
-        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Transform.Translate.html
-        transform.Translate(
-          translation
-        );
+            // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Rigidbody2D.MovePosition.html
+            rb2D.MovePosition(newPosition);
+        }
     }
 }
