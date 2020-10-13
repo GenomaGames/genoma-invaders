9737a494d222b2a96e99fea69eac5ed3d96c9d44
Alberto Fernandez <albertofdzm@gmail.com>
2020-10-13 20:49:39 +0200

    feat(ui): add Score Component

---
+++ Assets/Genoma Invaders/UI/Score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private int score = 0;
+    // https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/script-Text.html
+    private Text text;
+
+    // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.Awake.html
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.Start.html
+    private void Start()
+    {
+        text.text = score.ToString();
+    }
+
+    public void AddScore (int points)
+    {
+        score += points;
+
+        text.text = score.ToString();
+    }
+}
