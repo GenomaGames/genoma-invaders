cd2ee686e4e6a4ed035e40ea5f75b043987b433a
Alberto Fernandez <albertofdzm@gmail.com>
2020-10-13 20:51:18 +0200

    feat(enemy): update Enemy to update score on destroy

--- Assets/Genoma Invaders/Enemy/Enemy.cs
+++ Assets/Genoma Invaders/Enemy/Enemy.cs
@@ -6,11 +6,22 @@ public class Enemy : MonoBehaviour
 {
     // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Rigidbody2D.html
     private Rigidbody2D rb2D;
+    private Score score;
 
     // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.Awake.html
     private void Awake()
     {
         // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Component.GetComponent.html
         rb2D = GetComponent<Rigidbody2D>();
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Object.FindObjectOfType.html
+        score = FindObjectOfType<Score>();
+    }
+
+    // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/MonoBehaviour.OnDestroy.html
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy1");
+        score.AddScore(100); // Increment game score by 100
     }
 }
