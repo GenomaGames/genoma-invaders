c4e0e7c95c0b4707cf2478cf8def5b82a33309ba
Alberto Fernandez <albertofdzm@gmail.com>
2023-01-31 20:21:18 +0100

    🖼️ fix enemy sprite reference and background render configuration

--- Assets/Genoma Invaders/EnemyManager.cs
+++ Assets/Genoma Invaders/EnemyManager.cs
@@ -22,6 +22,16 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
+    private void OnDisable()
+    {
+        Enemy[] enemies = enemiesParent.GetComponentsInChildren<Enemy>();
+
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.OnDie -= OnEnemyDie;
+        }
+    }
+
     private void OnEnemyDie(Enemy enemy)
     {
         enemy.OnDie -= OnEnemyDie;
